a99f01350dd27d9d5aa48e59935079e8af99ac5d
Hugo <h@hugo.sx>
2017-01-15 21:06:31 +0100

    Fix viewport

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { Container } from 'semantic-ui-react';
 import Search from './SearchComponent';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    document.title = "FODMAP Search";
-  }
   render() {
     return (
       <div>
